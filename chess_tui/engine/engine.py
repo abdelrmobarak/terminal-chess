3c9452d5b50ead2db5a02fa0670f03e7c2f5ec40
abdelrmobarak <abdelrmobarak@gmail.com>
2026-01-05 10:36:33 -0500

    updated

--- chess_tui/engine/engine.py
+++ chess_tui/engine/engine.py
@@ -2,7 +2,7 @@ from stockfish import Stockfish
 from chess_tui.screens.settings import getSkill, getDepth
 
 def engine(): 
-    sfEngine = Stockfish(path="/Users/abdel-rahmanmobarak/Desktop/stockfish/stockfish-macos-m1-apple-silicon")
+    sfEngine = Stockfish(path="{YOUR PATH}")
     sfEngine.set_skill_level(getSkill())
     sfEngine.set_depth(getDepth())
     return sfEngine
